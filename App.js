56640c1f9a63b3f26ecbb24b9c1b33a524efadf8
David Werner <davidwerner2@gmx.de>
2022-01-23 18:58:07 +0200

    Added state variables

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState(0);
+  const [lower, setLower] = useState(0);
+  const [upper, setUpper] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text>Age</Text>
