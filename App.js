64bc53a452e16d8e487f550ee85c97a9598a27ef
David Werner <davidwerner2@gmx.de>
2022-01-23 18:40:32 +0200

    Implemented UI for the App

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age</Text>
+      <TextInput keyboardType='numeric'></TextInput>
+      <Text>Heart Rate Limits</Text>
+      <Text></Text>
+      <Button title='Calculate'></Button>
+      
     </View>
   );
 }
